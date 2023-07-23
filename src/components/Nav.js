c186aa720ad24c94c32f0f4160c95535d0cb89e6
corynko <ethankotel@gmail.com>
2023-07-23 10:58:32 -0600

    Initial commit, added themes and nav

---
+++ src/components/Nav.js
@@ -0,0 +1,76 @@
+import Link from "@mui/material/Link";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Box from "@mui/material/Box";
+import muiTheme from "./muiTheme";
+
+function Nav() {
+  return (
+    <Box
+      theme={muiTheme}
+      // display={"flex"}
+      // justify-content={"flex-end"}
+      sx={{
+        width: 3000,
+        height: 150,
+        backgroundColor: "primary.dark",
+        "&:hover": {
+          backgroundColor: "primary.main",
+        },
+      }}
+    >
+      <Breadcrumbs separator="||" size="lg">
+        <Link
+          to={"/who-i-am"}
+          underline="none"
+          level="h5"
+          className="nav-item custom-nav"
+          style={{ margin: "1rem" }}
+        >
+          who i am
+        </Link>
+        <Link
+          to={"/who-i-am"}
+          underline="none"
+          level="h5"
+          variant="primary"
+          className="nav-item custom-nav"
+          style={{ margin: "1rem" }}
+        >
+          what i do
+        </Link>
+        <Link
+          to={"/who-i-am"}
+          underline="none"
+          level="h5"
+          variant="primary"
+          className="nav-item custom-nav"
+          style={{ margin: "1rem" }}
+        >
+          contact me
+        </Link>
+        <Link
+          to={"/who-i-am"}
+          underline="none"
+          level="h5"
+          variant="primary"
+          className="nav-item custom-nav"
+          style={{ margin: "1rem" }}
+        >
+          photography
+        </Link>
+        <Link
+          to={"/who-i-am"}
+          underline="none"
+          level="h5"
+          variant="primary"
+          className="nav-item custom-nav"
+          style={{ margin: "1rem" }}
+        >
+          audio
+        </Link>
+      </Breadcrumbs>
+    </Box>
+  );
+}
+
+export default Nav;
