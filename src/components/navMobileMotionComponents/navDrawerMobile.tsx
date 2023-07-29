11a734f246d3bcbf1f2c4001e4bb3a68333c240e
corynko <ethankotel@gmail.com>
2023-07-28 18:22:42 -0600

    rollback, trying navlink method to fix 404s on page load

--- src/components/navMobileMotionComponents/navDrawerMobile.tsx
+++ src/components/navMobileMotionComponents/navDrawerMobile.tsx
@@ -2,181 +2,146 @@ import MenuIcon from "@mui/icons-material/Menu";
 import SwipeableDrawer from "@mui/material/SwipeableDrawer";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Button";
+import NavLink from "@mui/material/NavLink";
 import React, { useState } from "react";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
 
 function NavDrawerMobile() {
-  // const [openDrawer, setOpenDrawer] = useState<boolean | false>(false);
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-    null
-  );
+  const [openDrawer, setOpenDrawer] = useState<boolean | false>(false);
 
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event &&
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      } else if (
+        event &&
+        event.type === "keydown" &&
+        (event as React.KeyboardEvent).key === "Esc"
+      ) {
+        setOpenDrawer(false);
+      }
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  // const toggleDrawer =
-  //   (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
-  //     if (
-  //       event &&
-  //       event.type === "keydown" &&
-  //       ((event as React.KeyboardEvent).key === "Tab" ||
-  //         (event as React.KeyboardEvent).key === "Shift")
-  //     ) {
-  //       return;
-  //     } else if (
-  //       event &&
-  //       event.type === "keydown" &&
-  //       (event as React.KeyboardEvent).key === "Esc"
-  //     ) {
-  //       setOpenDrawer(false);
-  //     }
-
-  //     setOpenDrawer(true);
-  //     {
-  //       return;
-  //     }
-  //   };
+      setOpenDrawer(true);
+      {
+        return;
+      }
+    };
 
   const list = (
-    // <Box
-    //   sx={{ flexDirection: "column" }}
-    //   role="presentation"
-    //   onClick={toggleDrawer(true)}
-    //   onKeyDown={toggleDrawer(true)}
-    // >
-    //   <Button
-    //     onClick={() => setOpenDrawer(false)}
-    //     sx={{
-    //       color: "#F5F5F5",
-    //       display: "flex",
-    //       fontFamily: "Raleway",
-    //       marginX: "1rem",
-    //       marginY: 1,
-    //       fontWeight: 300,
-    //       maxHeight: "100px",
-    //       flexGrow: 1,
-    //       borderRadius: "20%",
-    //     }}
-    //     href="https://ethankotel.dev/"
-    //     fullWidth={true}
-    //   >
-    //     WHO I AM
-    //   </Button>
-
-    //   <Button
-    //     onClick={() => setOpenDrawer(false)}
-    //     sx={{
-    //       color: "#F5F5F5",
-    //       display: "flex",
-    //       fontFamily: "Raleway",
-    //       marginX: "1rem",
-    //       marginY: 1,
-    //       fontWeight: 300,
-    //       maxHeight: "100px",
-    //       flexGrow: 1,
-    //       borderRadius: "20%",
-    //     }}
-    //     href="https://ethankotel.dev/projects"
-    //     fullWidth={true}
-    //   >
-    //     WHAT I DO
-    //   </Button>
-
-    //   <Button
-    //     onClick={() => setOpenDrawer(false)}
-    //     sx={{
-    //       color: "#F5F5F5",
-    //       display: "flex",
-    //       fontFamily: "Raleway",
-    //       marginX: "1rem",
-    //       marginY: 1,
-    //       fontWeight: 300,
-    //       maxHeight: "100px",
-    //       flexGrow: 1,
-    //       borderRadius: "20%",
-    //     }}
-    //     href="https://ethankotel.dev/contact"
-    //     fullWidth={true}
-    //   >
-    //     CONTACT ME
-    //   </Button>
-
-    //   <Button
-    //     onClick={() => setOpenDrawer(false)}
-    //     sx={{
-    //       color: "#F5F5F5",
-    //       display: "flex",
-    //       fontFamily: "Raleway",
-    //       marginX: "1rem",
-    //       marginY: 1,
-    //       fontWeight: 300,
-    //       maxHeight: "100px",
-    //       flexGrow: 1,
-    //       borderRadius: "20%",
-    //     }}
-    //     href="https://ethankotel.photos"
-    //     fullWidth={true}
-    //   >
-    //     PHOTOS
-    //   </Button>
-
-    //   <Button
-    //     onClick={() => setOpenDrawer(false)}
-    //     sx={{
-    //       color: "#F5F5F5",
-    //       display: "flex",
-    //       fontFamily: "Raleway",
-    //       marginX: "1rem",
-    //       marginY: 1,
-    //       fontWeight: 300,
-    //       maxHeight: "100px",
-    //       flexGrow: 1,
-    //       borderRadius: "20%",
-    //     }}
-    //     href="https://www.ethankotel.com/"
-    //     fullWidth={true}
-    //   >
-    //     AUDIO
-    //   </Button>
-    // </Box>
-    <Menu
-      id="menu-appbar"
-      anchorEl={anchorElNav}
-      anchorOrigin={{
-        vertical: "bottom",
-        horizontal: "left",
-      }}
-      keepMounted
-      transformOrigin={{
-        vertical: "top",
-        horizontal: "left",
-      }}
-      open={Boolean(anchorElNav)}
-      onClose={handleCloseNavMenu}
-      sx={{
-        display: { xs: "block", md: "none" },
-      }}
+    <Box
+      sx={{ flexDirection: "column" }}
+      role="presentation"
+      onClick={toggleDrawer(true)}
+      onKeyDown={toggleDrawer(true)}
     >
-      <MenuItem key="whoIAm" onClick={handleCloseNavMenu}>
-        <Typography textAlign="center">Who I Am</Typography>
-      </MenuItem>
-    </Menu>
+      <NavLink
+        onClick={() => setOpenDrawer(false)}
+        sx={{
+          color: "#F5F5F5",
+          display: "flex",
+          fontFamily: "Raleway",
+          marginX: "1rem",
+          marginY: 1,
+          fontWeight: 300,
+          maxHeight: "100px",
+          flexGrow: 1,
+          borderRadius: "20%",
+        }}
+        to="/"
+        fullWidth={true}
+      >
+        WHO I AM
+      </NavLink>
+
+      <NavLink
+        onClick={() => setOpenDrawer(false)}
+        sx={{
+          color: "#F5F5F5",
+          display: "flex",
+          fontFamily: "Raleway",
+          marginX: "1rem",
+          marginY: 1,
+          fontWeight: 300,
+          maxHeight: "100px",
+          flexGrow: 1,
+          borderRadius: "20%",
+        }}
+        to="/projects"
+        fullWidth={true}
+      >
+        WHAT I DO
+      </NavLink>
+
+      <NavLink
+        onClick={() => setOpenDrawer(false)}
+        sx={{
+          color: "#F5F5F5",
+          display: "flex",
+          fontFamily: "Raleway",
+          marginX: "1rem",
+          marginY: 1,
+          fontWeight: 300,
+          maxHeight: "100px",
+          flexGrow: 1,
+          borderRadius: "20%",
+        }}
+        to="/contact"
+        fullWidth={true}
+      >
+        CONTACT ME
+      </NavLink>
+
+      <Button
+        onClick={() => setOpenDrawer(false)}
+        sx={{
+          color: "#F5F5F5",
+          display: "flex",
+          fontFamily: "Raleway",
+          marginX: "1rem",
+          marginY: 1,
+          fontWeight: 300,
+          maxHeight: "100px",
+          flexGrow: 1,
+          borderRadius: "20%",
+        }}
+        href="https://ethankotel.photos"
+        fullWidth={true}
+      >
+        PHOTOS
+      </Button>
+
+      <Button
+        onClick={() => setOpenDrawer(false)}
+        sx={{
+          color: "#F5F5F5",
+          display: "flex",
+          fontFamily: "Raleway",
+          marginX: "1rem",
+          marginY: 1,
+          fontWeight: 300,
+          maxHeight: "100px",
+          flexGrow: 1,
+          borderRadius: "20%",
+        }}
+        href="https://www.ethankotel.com/"
+        fullWidth={true}
+      >
+        AUDIO
+      </Button>
+    </Box>
   );
 
   return (
     <div>
-      <Button onClick={handleOpenNavMenu}>
+      <Button onClick={toggleDrawer(true)}>
         <MenuIcon className="menuIcon" />
       </Button>
 
-      {/* <SwipeableDrawer
+      <SwipeableDrawer
         open={openDrawer}
         onOpen={toggleDrawer(true)}
         onClose={toggleDrawer(false)}
@@ -186,9 +151,9 @@ function NavDrawerMobile() {
           display: { xs: "flex", md: "none" },
           flexDirection: "column",
         }}
-      > */}
-      {list}
-      {/* </SwipeableDrawer> */}
+      >
+        {list}
+      </SwipeableDrawer>
     </div>
   );
 }
